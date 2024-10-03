7b7343367ff2b7cfba426574d71c093d8746a946
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-03 14:46:16 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(3);
+// console.log(3);
+let a = 1;
+
+console.log(a);
+
+let b = a;
+
+console.log(b)
+
+c = b + 1;
+
+console.log(c)
+
+let d = [1, 2, 3]
+
+console.log(d)
+
+let e = d;
+
+console.log(e)
+
+e.push(4)
+
+console.log(e)
+
+console.log(d)
+
